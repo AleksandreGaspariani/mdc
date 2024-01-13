97fa31a39671af10e6d7bf3e48acf479d988e025
AleksandreGaspariani <aleksandregasparyan@gmail.com>
2024-01-13 04:22:56 +0400

    added default form for property

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ $(document).ready(()=> {
 })
 
 function closeFromGame(){
-    mp.trigger('hideMDC'); //testirebisas gadaxaze es metodi
+    // mp.trigger('hideMDC'); //testirebisas gadaxaze es metodi
 }
 
 function closeMDC(){
@@ -57,17 +57,20 @@ function init(){
 // }
 
 //** original purposes
-let playerData = [];
-let vehicleData = [];
+
+// let playerData = [];
+// let vehicleData = [];
+// let propertyData = [];
 
 //** testing purposes
-//let playerData = '{"FullName":"Veronica Woods","Age":35,"Sex":"მდედრობითი","Avatar":null,"PhoneNumber":0,"BornDate":"2023-06-24T00:00:00","IsWanted":true,"WantedText":"მკვლელობა დამამძიმებელ გარემოებებში","Licenses":["#5 | Weapon License - FPM | Due date 1/1/0001 12:00:00 AM","#4 | Weapon License - FPM | Due date 1/1/0001 12:00:00 AM","#3 | Weapon License - FPM | Due date 1/1/0001 12:00:00 AM","#2 | Weapon License - FPM | Due date 1/1/0001 12:00:00 AM","#1 | Driving | Due date 1/1/0001 12:00:00 AM"],"Vehicles":["[MUGALA] Mercedes-Benz CLS63 AMG (საჯარიმო სადგომზე 350$)","[MUGALA2] E39","[UENZBQ] Karin Dilettante (dilettante)","[9K8WFB] Invetero Coquette BlackFin (coquette3)","[TIWDCX] BF Club (club)","[2QMSCJ] Lampadati Komoda (komoda)","[T50XE3] Grotti Carbonizzare (carbonizzare)"],"Properties":["1. Paleto Blvd #2","2. Procopio Dr / Paleto Blvd #7"],"Records":["#1 | ძებნაშია | მკვლელობა დამამძიმებელ გარემოებებში","#2 | დაკავებულია | ყაჩაღობა","#3 | ანულირებულია (Thomas Anderson) | ჯგუფური თავდასხმა"],"Tickets":["#1 | გადაუხდელი | 200$ | imiromtom"],"Notes":["#1 | 12/28/2023 12:55:08 AM | მიეცა სიტყვიერი გაფრთხილება საგზაო მოძრაობის წესების დარღვევაზე"]}';
-//let vehicleData = '{"VINCode":"VIN05126694","OwnerName":"Veronica Woods","VehicleName":"Mercedes-Benz CLS63 AMG","VehicleCodeName":"cls2015","LicensePlate":"MUGALA","ColorCode1":0,"ColorCode2":0,"ImpoundPrice":350,"ImpoundReason":"სატესტო","PreviousOwners":["1. Pearce Jackson - 8/14/2019 6:02:06 PM","2. Jonathan Woods - 12/1/2016 6:01:44 PM"],"AssignedTo":[]}';
+// let playerData = '{"FullName":"Veronica Woods","Age":35,"Sex":"მდედრობითი","Avatar":null,"PhoneNumber":0,"BornDate":"2023-06-24T00:00:00","IsWanted":true,"WantedText":"მკვლელობა დამამძიმებელ გარემოებებში","Licenses":["#5 | Weapon License - FPM | Due date 1/1/0001 12:00:00 AM","#4 | Weapon License - FPM | Due date 1/1/0001 12:00:00 AM","#3 | Weapon License - FPM | Due date 1/1/0001 12:00:00 AM","#2 | Weapon License - FPM | Due date 1/1/0001 12:00:00 AM","#1 | Driving | Due date 1/1/0001 12:00:00 AM"],"Vehicles":["[MUGALA] Mercedes-Benz CLS63 AMG (საჯარიმო სადგომზე 350$)","[MUGALA2] E39","[UENZBQ] Karin Dilettante (dilettante)","[9K8WFB] Invetero Coquette BlackFin (coquette3)","[TIWDCX] BF Club (club)","[2QMSCJ] Lampadati Komoda (komoda)","[T50XE3] Grotti Carbonizzare (carbonizzare)"],"Properties":["1. Paleto Blvd #2","2. Procopio Dr / Paleto Blvd #7"],"Records":["#1 | ძებნაშია | მკვლელობა დამამძიმებელ გარემოებებში","#2 | დაკავებულია | ყაჩაღობა","#3 | ანულირებულია (Thomas Anderson) | ჯგუფური თავდასხმა"],"Tickets":["#1 | გადაუხდელი | 200$ | imiromtom"],"Notes":["#1 | 12/28/2023 12:55:08 AM | მიეცა სიტყვიერი გაფრთხილება საგზაო მოძრაობის წესების დარღვევაზე"]}';
+// let vehicleData = '{"VINCode":"VIN05126694","OwnerName":"Veronica Woods","VehicleName":"Mercedes-Benz CLS63 AMG","VehicleCodeName":"cls2015","LicensePlate":"MUGALA","ColorCode1":0,"ColorCode2":0,"ImpoundPrice":350,"ImpoundReason":"სატესტო","PreviousOwners":["1. Pearce Jackson - 8/14/2019 6:02:06 PM","2. Jonathan Woods - 12/1/2016 6:01:44 PM"],"AssignedTo":[]}';
+// let propertyData = '{"VINCode":"VIN05126694","OwnerName":"Veronica Woods","VehicleName":"Mercedes-Benz CLS63 AMG","VehicleCodeName":"cls2015","LicensePlate":"MUGALA","ColorCode1":0,"ColorCode2":0,"ImpoundPrice":350,"ImpoundReason":"სატესტო","PreviousOwners":["1. Pearce Jackson - 8/14/2019 6:02:06 PM","2. Jonathan Woods - 12/1/2016 6:01:44 PM"],"AssignedTo":[]}';
 
 function handlePlayerInput() {
     state = 'player'
     let user = $('#player_input').val(); // -> user name which was requested.
-    mp.trigger('MDC_RequestPlayerData', user); //testirebisas gadaxaze es method
+    // mp.trigger('MDC_RequestPlayerData', user); //testirebisas gadaxaze es method
     onRecievePlayerData(playerData); // <- pass data here
 }
 
@@ -123,7 +126,7 @@ function onRecievePlayerData(data){
 function handleVehicleInput() {
     state = 'vehicle';
     let vehicle = $('#vehicle_input').val(); // -> vehicle license plate which was requested.
-    mp.trigger('MDC_RequestVehicleData', vehicle); //testirebisas gadaxaze es metodi
+    // mp.trigger('MDC_RequestVehicleData', vehicle); //testirebisas gadaxaze es metodi
     onRecieveVehicleData(vehicleData); // <- pass data here
 }
 
@@ -173,10 +176,29 @@ function onRecieveVehicleData(data){
     }
 }
 
+function handlePropertyInput() {
+    state = 'property';
+    let propertyAddress = $('#property_input').val(); // -> vehicle license plate which was requested.
+    // mp.trigger('MDC_RequestPropertyData', propertyAddress); //testirebisas gadaxaze es metodi
+    onRecievePropertyData(propertyData); // <- pass data here
+}
+
 function onRecievePropertyData(data){
+    propertyData = data;
     propertyInfo = [];
-    let jsonData = JSON.parse(data);    
-    propertyInfo = jsonData;
+    
+    if (data.length < 1) {
+        load();
+        console.log('length less than 1 character');
+    }else{
+        console.log('test');
+        let jsonData = JSON.parse(data);    
+        propertyInfo = jsonData;
+        load();
+
+        // insert Data.
+    }
+
 }
 
 function onRecieveCallsData(data){
@@ -436,10 +458,101 @@ function handle(param) {
 
                     </div>
 
-                    </div>
+                </div>
                 `)
             }
             break;
+        case 'property':
+            if (propertyInfo.length < 1) {
+                $('#content').empty().append(`
+                <div class='d-flex flex-column align-items-center justify-content-center'>
+                    <div class='d-flex justify-content-center align-items-center m-5' style='gap: 5px;'> 
+                        <label for=''>Find Property by Address</label>
+                        <input type='text' placeholder='Type Address' class='form-input' id='property_input'>
+                        <span class='material-symbols-outlined m-0 p-0' id='find_property_button' onclick='handlePropertyInput()' style='cursor: pointer'>
+                            chevron_right
+                        </span>
+                    </div>
+                </div> 
+                `)
+            }else {
+                $('#content').empty().append(`
+                    <div class='d-flex flex-column justify-content-start align-items-start'>
+                        <div class='w-100'>
+                            <p class='d-flex justify-content-start text-danger align-items-center' onclick='goToMain()' style='cursor:pointer'>
+                                Go Back
+                                <span class='material-symbols-outlined' id='find_property_button' onclick='handlePropertyInput()' style='cursor: pointer'>
+                                    reply
+                                </span>
+                            </p>
+                        </div>
+
+                        <div class='d-flex w-100 flex-row align-items-center justify-content-center m-5 ms-0'>
+                            <div class='w-75 d-flex flex-column'>
+                                <label for='' class='text-start'>Find Property information by address</label>
+                                <div class='d-flex justify-content-start align-items-center w-50'>
+                                    <input type='text' placeholder='Type Address' class='form-control' id='property_input' style='width: 100%;'>
+                                    <span class='material-symbols-outlined m-0 p-0' id='find_vehicle_button' onclick='handleVehicleInput()' style='cursor: pointer'>
+                                        chevron_right
+                                    </span>
+                                </div>
+                            </div>
+                            <div class='w-25 d-flex justify-content-center align-items-center'>
+                                <button class='btn btn-outline-info'>Mark Location</button>
+                            </div>
+                        </div> 
+                    </div>
+                
+                    <hr>
+
+                    <div class='w-100'>
+                        <h4>Information: </h4>
+                        <ul class='m-0 p-0'>
+                            <li>Property Address: <span class='text-info' id='propertyAddress'></span></li>
+                            <li>Owner: <span class='text-info' id='propertyOwner'></span></li>
+                        </ul>
+                    </div>
+
+                    <div class='d-flex w-100'>
+
+                        <div class='d-flex flex-column w-50'>
+
+                            <div class='d-flex justify-content-start align-items-start flex-column mt-3'>
+                                <h4 class='mb-3'>Property Tenants : </h4>
+                                <div style='max-height: 120px;' class='c_scroll'>
+                                    <ul class='m-0 p-0 ms-5 d-flex flex-column justify-content-start align-items-start' style='width: 300px; list-style-type: decimal-leading-zero;' id='vehicleAssignedTo'>
+                                        
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class='d-flex justify-content-start align-items-start flex-column mt-3'>
+                                <h4 class='mb-3'>Previous Owners: </h4>
+                                <div style='max-height: 70px;' id='records-out-div' class='c_scroll'>
+                                    <ul class='m-0 p-0 ms-2 d-flex flex-column justify-content-start align-items-start' style='width: 300px; list-style-type: none;' id='vehiclePreviousOwners'>
+                                        
+                                    </ul>
+                                </div>
+                            </div>
+                            
+                        </div>
+
+                        <div class='d-flex flex-column w-50'>
+
+                            <div class='d-flex justify-content-start align-items-start flex-column mt-3'>
+                                <h4 class='mb-3'>Warrant Information: </h4>
+                                <div style='max-height: 120px;' class='c_scroll'>
+                                    <ul class='m-0 p-0 ms-2 d-flex flex-column justify-content-start align-items-start' style='width: 300px; list-style-type: decimal-leading-zero;' id='vehicleImpound'>
+
+                                    </ul>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                `)
+            }
+        break;
     
         default:
             $('#content').empty().append(`
